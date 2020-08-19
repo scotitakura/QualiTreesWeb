ad39a7f4959355e13e8e0582d0a78c5e54ddf00e
sitakura <scot.itakura@gmail.com>
2020-08-19 16:13:43 -0700

    skeleton website with firebase and materialize setup

---
+++ README.md
@@ -0,0 +1,9 @@
+#QualiTrees
+notes:
+ * Space theme, tree theme?
+ * Logo (&logo text) white space
+ * Logo text only
+ * Same page
+ * Example client requirements
+ * Tabs
+ * Animations?
---
+++ public/assets/js/script.js
@@ -0,0 +1,9 @@
+M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.scrollspy');
+//   var instances = M.ScrollSpy.init(elems);
+// });
